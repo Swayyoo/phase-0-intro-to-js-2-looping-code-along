8428374b3c2ae8ef35ed01148a3a1751e167459f
Josue Garcia <jgws03@gmail.com>
2023-01-11 18:52:23 -0500

    commit tree 38ce35cd5a585907aef18adc1d919ce519835b2d

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+
+const names = ["Guadalupe", "Ollie", "Aki"]; 
+function writeCards( names, event ) { 
+    let completedCards = []
+    for(let i = 0; i < names.length; i++) { 
+        completedCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        console.log(completedCards)
+    }
+    return completedCards;
+}
+writeCards(names, "Surprise"); 
+
+function countDown() { 
+    let countDown = 10;
+    while(countDown >= 0)
+    console.log(countDown--);
+}
